a1b1b161f221b227799958e8846a7e088a7c67a5
AkWinchester <arsenijkiselev03@gmail.com>
2023-06-25 20:39:03 +0300

    наша регистрация

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from urllib.parse import parse_qs
+
+from flask import Flask, request
 from flask_restful import Api, Resource
+from models.user import add_user, check_user
+import hashlib
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,13 +12,32 @@ api = Api(app)
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
     return response
 
 class DataResource(Resource):
     def get(self):
-        data = {"message": "hellow"}
+        data = {"message":"Hello, GET request!"}
         return data
+
+    def post(self):
+        # email = request.form.get('email')
+        # password = request.form.get('password')
+        data = request.get_json()
+
+        email = data['email']
+        password = data['password']
+
+        print("Email:", email)
+        print("Password:", password)
+
+        hash_password = hashlib.sha256(password.encode()).hexdigest()
+        if check_user(email):
+            add_user(email, hash_password)
+            return {"message": "user added successfully"}
+        else:
+            return {"message": "there is already a user with this email"}
+
 
 api.add_resource(DataResource, '/api/data')
 
--- models/models.py
+++ models/models.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from config import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME
-from flask_security import UserMixin
-
+from datetime import datetime
 # Создание подключения к базе данных
 engine = create_engine(f'mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
 
@@ -15,27 +14,9 @@ session = Session()
 Base = declarative_base()
 
 
-class User(Base, UserMixin):
+class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
-    active = Column(Boolean, default=True)
-    confirmed_at = Column(DateTime)
-    roles = relationship('Role', secondary='user_roles')
-
-
-class Role(Base):
-    __tablename__ = 'role'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), unique=True)
-
-
-class UserRoles(Base):
-    __tablename__ = 'user_roles'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    role_id = Column(Integer, ForeignKey('role.id'))
