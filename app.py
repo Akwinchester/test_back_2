457bf926fe5341cfbc3b6196e6f87135cfabc704
AkWinchester <arsenijkiselev03@gmail.com>
2023-06-25 14:11:26 +0300

    модель данных от gpt и рабочий app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    return response
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+class DataResource(Resource):
+    def get(self):
+        data = {"message": "hellow"}
+        return data
 
+api.add_resource(DataResource, '/api/data')
 
 if __name__ == '__main__':
     app.run()
